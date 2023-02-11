bf4f3da7dd7f8e0156ec0d88080745f11c3af7c5
graham-25 <grbills29@googlemail.com>
2023-02-11 10:53:33 +0000

    first function where the user selects what they want to do.

--- run.py
+++ run.py
@@ -8,10 +8,30 @@ holding the answers to see if the user has guessed a letter in the word or not.
 """
 
 def main_menu():
+    """
+    
+    """
     print("Welcome to Hang-Man, the aim of the game is to guess the work before the man meets the galows! \n")
     print("Please select one of the following! \n")
-    print("Start Game \n Read Instructions \n Quit Game \n")
-    start_game()
-    read_instructions()
-    quit_game()
+    print("\n Start Game: 1\n Read Instructions: 2\n Quit Game: 3\n")
+    option = int(input("Please select a game option! \n"))
+    if option == 1:
+        start_game()
+    elif option == 2:
+        read_instructions()
+    elif option == 3:
+        quit_game()
 
+def start_game():
+    """
+    This function starts the game,     
+    """
+    choice_words = ['octipuss', 'banana', 'frog']
+def option():
+    """
+    This function tells the user how to play the game,
+    """
+def quit_game():
+    """
+    This function quits the game.
+    """
